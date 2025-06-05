1a8832af027c3aed04c13f51f0ea5efa51a9cf45
Moa Magnfors <moa@magnfors.se>
2025-06-05 20:28:38 +0200

    ✨ feat: add header and styling

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { TodoList } from './components/TodoList';
 function App() {
   return (
     <>
+      <h2 className="text-3xl font-bold text-black-300 mt-8 mb-8 uppercase">
+        Innan sommarledighet:
+      </h2>
       <TodoList />
     </>
   );
